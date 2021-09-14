3ec8bd9c9c30d9692612aff02058de10a9728832
Eva Roets <84382812+EvaRoets@users.noreply.github.com>
2021-09-14 16:07:12 +0200

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+    // the file that will be looked at first
+    entry: './src/index.ts',
+    // transport TS into JS
+    module: {
+        rules: [
+            {
+                //if file passes this test -being .ts- it well be used, if not, nothing happens
+                test: /\.ts$/,
+                use: 'ts-loader',
+                include: [path.resolve(__dirname, 'src')],
+                exclude: /node_modules/,
+            },
+        ],
+    // the place where the compiled code will be sent
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public'),
+    },
+    mode: 'development',
+
+    },
+
+}
